e36336bf937639eae8039901a76a4e210eb8cd7b
RicardoJGC10 <chatotote10@gmail.com>
2024-06-10 17:25:00 -0600

    Login Terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './layout/publico/login/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { PrincipalComponent } from './layout/privado/principal/principal.component';
+import { AutentifacionService } from './shared/servicios/autentifacion.service';
+import { CommonModule } from '@angular/common';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, FormsModule,PrincipalComponent,CommonModule,UsuariosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'login-angular';
+
+  constructor(private loginPrd:AutentifacionService) {}
+
+  public visualizarMenu():boolean{
+    return this.loginPrd.habilitarlogeo();
+
+  }
 }
---
+++ src/app/layout/publico/login/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AutentifacionService } from '../../../../shared/servicios/autentifacion.service';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+
+  public myform!:FormGroup;
+
+  constructor(private fb:FormBuilder, private loginPrd:AutentifacionService) {}
+
+  ngOnInit(): void {
+    this.myform = this.createMyForm();
+    
+  }
+  private createMyForm():FormGroup{
+    return this.fb.group({
+      name:['',[Validators.required]],
+      username:['',Validators.required]
+    });
+  }
+  public submitFormulario(){
+    if(this.myform.invalid){
+      Object.values(this.myform.controls).forEach(control=>{
+        control.markAllAsTouched();
+      });
+      return;
+    }
+
+    if(!this.loginPrd.ingresarAplicativo(this.myform.value)){
+      alert("Usuario o Contraseña Invalido");
+    }
+
+  }
+  public get f():any{
+    return this.myform.controls;
+  }
+
+}
