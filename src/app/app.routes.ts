081d3946cf363a80e39ff39a2e98500c326fc016
RicardoJGC10 <chatotote10@gmail.com>
2024-07-05 01:49:34 -0600

    Cambio7

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,18 @@ import { DashboardComponent } from './layout/dashboard/dashboard/dashboard.compo
 import { EstadisticasComponent } from './layout/estadisticas/estadisticas/estadisticas.component';
 import { AuthGuard } from './auth.guard';
 import { FooterComponent } from './layout/footer/footer/footer.component';
+import { HomeComponent } from './layout/inicio/home/home.component';
+import { CategoriasComponent } from './layout/categorias/categorias/categorias.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard] },
   { path: 'tarjetas', component: FooterComponent, canActivate:[AuthGuard] },
   { path: 'estadisticas', component: EstadisticasComponent, canActivate:[AuthGuard] },
+  { path: 'categorias', component: CategoriasComponent, canActivate:[AuthGuard] },
+
 
 
   // Otras rutas aquí
