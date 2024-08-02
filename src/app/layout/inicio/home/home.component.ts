681578c933b26c3dff6d4132f7396883ad124459
RicardoJGC10 <chatotote10@gmail.com>
2024-08-01 22:33:32 -0600

    Mostrar Productos con Imagen en la Página Web de la Empresa

--- src/app/layout/inicio/home/home.component.ts
+++ src/app/layout/inicio/home/home.component.ts
@@ -1,24 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductosInterface } from '../../../modelos/productos.interface';
+import { CommonModule } from '@angular/common';
+import { ProductoService } from '../../servicios/producto.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  productos: ProductosInterface[] = [];
+  baseUrl = 'http://143.198.70.194/'; // Asegúrate de que sea la URL base correcta
 
-  constructor() { }
+  constructor(private productosService: ProductoService) { }
 
-    ngOnInit(): void {
+  ngOnInit(): void {
+    this.getProductos();
+  }
+
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
     }
-  
-    scrollToSection(sectionId: string): void {
-      const element = document.getElementById(sectionId);
-      if (element) {
-        element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+  }
+
+  getProductos(): void {
+    this.productosService.getCategorias().subscribe({
+      next: (result) => {
+        this.productos = result.map(producto => ({
+          ...producto,
+          image: this.baseUrl + producto.image
+        }));
+        console.log(this.productos); // Verifica las URLs de las imágenes
+      },
+      error: (err) => {
+        console.error('Error fetching products:', err);
       }
-    }
-
+    });
+  }
 }
